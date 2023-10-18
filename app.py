0c822e51fce27660ebf57001a6e568d68e0f6028
judenilson <judenilson@hotmail.com>
2023-10-18 20:59:00 -0300

    Add Navigation and Routing

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ def main(page: ft.Page):
     page.theme = ft.Theme(
         color_scheme_seed="#5739ef",
     )
+    # page.theme_mode = "LIGHT"
+    # page.theme_mode = "DARK"
 
     def slope_calculate(e):
         if rwy_in_use.value == "" or opposit.value == "" or length_rwy_in_use.value == "":
@@ -85,11 +87,11 @@ def main(page: ft.Page):
     def route(e):
         page.views.clear()
 
-# objetos ------------------------
-    # Titulo -------------------------
+# Main Page Objects------------------------
+    # Title -------------------------
     title = ft.Text(value="TakeOff Performance", style="displaySmall", )
     
-    # Seção do Slope -------------------------
+    # Slope Section -------------------------
     rwy_in_use = ft.TextField(label="THR elevation in use", suffix_text="ft", width=150, max_length=5, on_change=validate_only_real_number)
     opposit = ft.TextField(label="Opposit THR elevation", suffix_text="ft", width=150, max_length=5, on_change=validate_only_real_number)
     length_rwy_in_use = ft.TextField(label="Length of RWY in use", suffix_text="ft", width=150, max_length=5, on_change=validate_only_positive_number)
@@ -97,7 +99,7 @@ def main(page: ft.Page):
     slope_button_reset = ft.OutlinedButton("Reset", on_click=slope_reset)
     slope_result = ft.TextField(label="SLOPE", bgcolor=ft.colors.SECONDARY_CONTAINER, read_only=True, width=150, suffix_text="%", text_size=30)
 
-    # Seção do Clima -------------------------
+    # Weather Section -------------------------
     wind_direction = ft.TextField(label="Wind Direction", suffix_text="°", width=150, max_length=3, on_change=validate_degrees)
     wind_speed = ft.TextField(label="Wind Speed", suffix_text="kt", width=150, max_length=2, on_change=validate_only_positive_number)
     oat = ft.TextField(label="OAT", suffix_text="°C", width=150, max_length=3, on_change=validate_only_real_number)
@@ -114,7 +116,7 @@ def main(page: ft.Page):
             ft.dropdown.Option("Comp. Snow"),
         ])
 
-    # Seção da Aeronave -------------------------
+    # Aircraft Section -------------------------
     tow = ft.TextField(label="TOW", suffix_text="Lb", width=100, on_change=validate_only_positive_number)
     tocg = ft.TextField(label="TOCG", suffix_text="%", width=100, on_change=validate_only_positive_number)
     thrust = ft.Dropdown(
@@ -145,15 +147,26 @@ def main(page: ft.Page):
         label_position=ft.LabelPosition.LEFT
         )
     
-    run_button = ft.FilledButton("RUN", on_click="")
+    run_button = ft.FilledButton("RUN", on_click=lambda _: page.go("/result"), width=300, height=70)
 
-# Render ------------------------
-    page.add(
+# Result Page Objects------------------------
+
+    r_config = ft.TextField(label="Configuration")
+    r_thrust = ft.TextField(label="Thrust")
+    r_v1 = ft.TextField(label="V1")
+    r_vr = ft.TextField(label="VR")
+    r_v2 = ft.TextField(label="V2")
+    r_trim = ft.TextField(label="Trim")
+    return_button = ft.ElevatedButton("Return", on_click=lambda _: page.go("/"))
+
+# Pages ------------------------
+    main_page = [
         ft.Container(
             title,
             alignment=ft.alignment.top_center,
-            margin=10
+            margin=20
         ),
+        ft.Divider(),
         ft.Row(
             [
                 rwy_in_use,
@@ -171,6 +184,7 @@ def main(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.END
         ),
+        ft.Divider(),
         ft.Row(
             [
                 wind_direction,
@@ -188,7 +202,8 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.SPACE_AROUND,
             vertical_alignment=ft.CrossAxisAlignment.START,
             run_spacing=10,
-        ),
+        ),        
+        ft.Divider(),
         ft.Row(
             [
                 tow,
@@ -207,9 +222,60 @@ def main(page: ft.Page):
             alignment=ft.MainAxisAlignment.END,
             run_spacing=10,
         ),
-        run_button
-    )
+        ft.Container(
+            run_button,
+            alignment=ft.alignment.center,
+            margin=30
+        )
+    ]
 
+    result_page = [
+        ft.Container(
+            title,
+            alignment=ft.alignment.top_center,
+            margin=20
+        ),
+        ft.Divider(),
+        r_config,
+        r_thrust,
+        r_v1,
+        r_vr,
+        r_v2,
+        r_trim,
+        ft.Container(
+            return_button,
+            alignment=ft.alignment.center,
+            margin=30
+        ),
+    ]
+
+# Navigation and routing ------------------------
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/",
+                main_page,
+            )
+        )
+        if page.route == "/result":
+            page.views.append(
+                ft.View(
+                    "/result",
+                    result_page,
+                )
+            )
+        page.update()
+    
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+    
 
 ft.app(target=main)
 # ft.app(target=main, view=ft.WEB_BROWSER, port=8550) # Executar o app no Browser
